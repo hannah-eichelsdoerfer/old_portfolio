868a156193a6004ab3a1b70b4839a44baf04822d
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2021-06-16 12:20:53 +0200

    Basic layout started

---
+++ js/components/header.js
@@ -0,0 +1,9 @@
+import Typed from 'typed.js';
+
+var options = {
+  strings: ['Full-Stack Developer', 'Front-End Developer', 'Back-End Developer'],
+  typeSpeed: 100,
+  loop: true
+};
+
+var typed = new Typed('#header-typed', options);
