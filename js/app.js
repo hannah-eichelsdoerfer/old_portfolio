426551be22bb5a9136b2b4532ce1d3562bf18952
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2021-07-03 22:53:05 +0200

    Mobile Modal

--- js/app.js
+++ js/app.js
@@ -44,8 +44,7 @@ let typed = new Typed(header, {
   typeSpeed: 90,
 });
 
-
-// Cards Modal Popup
+// MODALS ON CARDS
 const modals = document.querySelectorAll(".modal");
 const overlay = document.querySelector(".overlay");
 const closeBtns = document.querySelectorAll(".close-modal");
@@ -53,29 +52,34 @@ const projectCards = document.querySelectorAll(".project-card");
 
 const openModal = (modal) => {
   // modal.classList.remove("hidden");
-  // overlay.classList.remove("hidden");
   const data = document.getElementById(`${modal}Modal`);
   data.classList.remove("hidden");
   overlay.classList.remove("hidden");
-}
+};
 
-const closeModal = (target) => {
-  target.classList.add("hidden");
+const closeModal = () => {
+  modals.forEach ((modal) => {if (!modal.classList.contains("hidden")) modal.classList.add("hidden")});
   overlay.classList.add("hidden");
-}
+};
 
-projectCards.forEach(card => {
+// const closeModal = (target) => {
+//   target.classList.add("hidden");
+// };
+
+projectCards.forEach((card) => {
   card.addEventListener("click", (event) => {
     openModal(event.currentTarget.id);
   });
 });
 
-closeBtns.forEach (closeBtn => {
-  closeBtn.addEventListener("click", (event) => {
-    closeModal(event.currentTarget.parentNode);
-  });
-  
-})
-overlay.addEventListener("click", (event) => {
-  modals.forEach ((modal) => {closeModal(modal)});
+closeBtns.forEach((closeBtn) => {
+  closeBtn.addEventListener("click", closeModal);
+});
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    closeModal();
+  }
 });
